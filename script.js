608cdbd9bbcb8a11d49c9043e12db98b80d6084c
NirKunwar6 <hamrosh59@gmail.com>
2026-03-25 11:56:00 +0545

    Create script.js

---
+++ script.js
@@ -0,0 +1,30 @@
+const GITA = [
+{
+chapter: 2,
+verse: 47,
+sanskrit: "कर्मण्येवाधिकारस्ते मा फलेषु कदाचन।",
+meaning_np: "तिमीलाई कर्म गर्ने अधिकार छ, तर फलमा कहिल्यै अधिकार छैन।",
+keywords: ["काम", "कर्तव्य", "success", "result", "future"]
+},
+{
+chapter: 2,
+verse: 14,
+sanskrit: "मात्रास्पर्शास्तु कौन्तेय शीतोष्णसुखदुःखदाः।",
+meaning_np: "सुख र दुःख अस्थायी हुन्छन्, तिनीहरू आउँछन् र जान्छन्।",
+keywords: ["दुःख", "pain", "sad", "emotion"]
+},
+{
+chapter: 6,
+verse: 5,
+sanskrit: "उद्धरेदात्मनात्मानं नात्मानमवसादयेत्।",
+meaning_np: "मानिसले आफैलाई माथि उठाउनुपर्छ, आफैलाई तल झार्नु हुँदैन।",
+keywords: ["motivation", "lazy", "fail", "self"]
+},
+{
+chapter: 18,
+verse: 66,
+sanskrit: "सर्वधर्मान्परित्यज्य मामेकं शरणं व्रज।",
+meaning_np: "सबै चिन्ता छोडेर मेरो शरणमा आऊ, म तिमीलाई मुक्त गर्नेछु।",
+keywords: ["fear", "worry", "stress"]
+}
+];
